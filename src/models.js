9b7e9c2f99d2097bc3ff5fcecb937ecb09118590
premultiply <4681172+premultiply@users.noreply.github.com>
2024-01-11 03:56:40 +0100

    update, add and fix list of camera models and series

--- src/models.js
+++ src/models.js
@@ -2,38 +2,44 @@ import { c } from './choices.js'
 
 export const MODELS = [
 	{ id: 'Auto', series: 'Auto', label: 'Auto Detect' },
-	{ id: 'AW-HE2', series: 'AW-HE2', label: 'AW-HE2' },
+	{ id: 'AW-HE2', series: 'HE2', label: 'AW-HE2' },
+	{ id: 'AW-HE20', series: 'UE20', label: 'AW-HE20' },
 	{ id: 'AW-HE35', series: 'HE40', label: 'AW-HE35' },
-	{ id: 'AW-HE38', series: 'HE40', label: 'AW-HE48' },
+	{ id: 'AW-HE38', series: 'HE40', label: 'AW-HE38' },
 	{ id: 'AW-HE40', series: 'HE40', label: 'AW-HE40' },
 	{ id: 'AW-HE42', series: 'HE42', label: 'AW-HE42' },
 	{ id: 'AW-HE48', series: 'HE40', label: 'AW-HE48' },
-	{ id: 'AW-HE50', series: 'AW-HE50', label: 'AW-HE50' },
+	{ id: 'AW-HE50', series: 'HE50', label: 'AW-HE50' },
 	{ id: 'AW-HE58', series: 'HE40', label: 'AW-HE58' },
-	{ id: 'AW-HE60', series: 'AW-HE60', label: 'AW-HE60' },
+	{ id: 'AW-HE60', series: 'HE60', label: 'AW-HE60' },
 	{ id: 'AW-HE65', series: 'HE40', label: 'AW-HE65' },
 	{ id: 'AW-HE68', series: 'HE42', label: 'AW-HE68' },
 	{ id: 'AW-HE70', series: 'HE40', label: 'AW-HE70' },
 	{ id: 'AW-HE75', series: 'HE42', label: 'AW-HE75' },
-	{ id: 'AW-HE120', series: 'AW-HE120', label: 'AW-HE120' },
-	{ id: 'AW-HE130', series: 'AW-HE130', label: 'AW-HE130' },
-	{ id: 'AW-HR140', series: 'AW-HR140', label: 'AW-HR140' },
+	{ id: 'AW-HE120', series: 'HE120', label: 'AW-HE120' },
+	{ id: 'AW-HE130', series: 'HE130', label: 'AW-HE130' },
+	{ id: 'AW-HE145', series: 'UE150', label: 'AW-HE145' },
 	{ id: 'AW-HN38', series: 'HE40', label: 'AW-HN38' },
 	{ id: 'AW-HN40', series: 'HE40', label: 'AW-HN40' },
 	{ id: 'AW-HN65', series: 'HE40', label: 'AW-HN65' },
-	{ id: 'AW-HN70', series: 'HE40', label: 'AW-HN70' },
-	{ id: 'AW-UE4', series: 'AW-UE4', label: 'AW-UE4' },
+	{ id: 'AW-HN130', series: 'HE130', label: 'AW-HN130' },
+	{ id: 'AW-HR140', series: 'HR140', label: 'AW-HR140' },
+	{ id: 'AW-UE4', series: 'UE4', label: 'AW-UE4' },
+	{ id: 'AW-UE20', series: 'UE20', label: 'AW-UE20' },
+	{ id: 'AW-UE40', series: 'UE80', label: 'AW-UE40' },
+	{ id: 'AW-UE50', series: 'UE80', label: 'AW-UE50' },
 	{ id: 'AW-UE63', series: 'UE70', label: 'AW-UE63' },
 	{ id: 'AW-UE65', series: 'UE70', label: 'AW-UE65' },
 	{ id: 'AW-UE70', series: 'UE70', label: 'AW-UE70' },
+	{ id: 'AW-UE80', series: 'UE80', label: 'AW-UE80' },
 	{ id: 'AW-UE100', series: 'UE150', label: 'AW-UE100' },
 	{ id: 'AW-UE150', series: 'UE150', label: 'AW-UE150' },
 	{ id: 'AW-UE155', series: 'UE150', label: 'AW-UE155' },
+	{ id: 'AW-UE160', series: 'UE160', label: 'AW-UE160' },
 	{ id: 'AW-UN70', series: 'UE70', label: 'AW-UN70' },
 	{ id: 'AW-UN145', series: 'UE150', label: 'AW-UN145' },
-	{ id: 'AW-HEF5', series: 'AW-HEF5', label: 'AW-HEF5' },
-	{ id: 'AW-SFU01', series: 'AW-SFU01', label: 'AW-SFU01' },
-	{ id: 'AK-UB300', series: 'AK-UB300', label: 'AK-UB300' },
+	{ id: 'AW-UR100', series: 'UE150', label: 'AW-UR100' },
+	{ id: 'AK-UB300', series: 'UB300', label: 'AK-UB300' },
 	{ id: 'AG-CX350', series: 'CX350', label: 'AG-CX350' },
 	{ id: 'AG-CX200', series: 'CX350', label: 'AG-CX200' },
 	{ id: 'AJ-UPX360', series: 'CX350', label: 'AJ-UPX360' },
@@ -44,20 +50,21 @@ export const MODELS = [
 
 // list of all Series:
 // Other
+// HE2
 // HE40
 // HE42
+// HE50
+// HE60
+// HE120
+// HE130
+// UE20
 // UE70
+// UE80
 // UE150
-// AW-HE2
-// AW-HE50
-// AW-HE60
-// AW-HE120
-// AW-HE130
-// AW-HR140
-// AW-HEF5
-// AW-UE4
-// AW-SHU01
-// AK-UB300
+// UE160
+// HR140
+// UE4
+// UB300
 // CX350
 
 export const SERIES_SPECS = [
@@ -101,8 +108,8 @@ export const SERIES_SPECS = [
 			OTAF: true, // Has One Touch Auto Focus Support (OSE:69:1)
 			iris: true, // Has Iris Support (manual and auto) (Ixx)
 			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_OTHER() }, // Has Gain Support
-			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_OTHER }, // Has Shutter Support
-			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_OTHER() }, // Has Pedistal Support
+			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_OTHER() }, // Has Shutter Support
+			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_OTHER() }, // Has Pedestal Support
 			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_OTHER() }, // Has ND Filter Support
 			preset: true, // Can Save and Recall Presets (Mxxx or Rxxx)
 			speedPset: true, // Has Preset Recall Speed Control (UPVSxx)
@@ -209,10 +216,10 @@ export const SERIES_SPECS = [
 			OAF: true,
 			OTAF: true,
 			iris: true,
-			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HE42() },
+			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HE40 },
 			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_HE42() },
-			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE42() },
-			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_HE42() },
+			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE40() },
+			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_3A },
 			preset: true,
 			speedPset: true,
 			timePset: false,
@@ -263,10 +270,10 @@ export const SERIES_SPECS = [
 			OAF: true,
 			OTAF: true,
 			iris: true,
-			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_UE70() },
+			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HE40 },
 			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_UE70() },
-			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_UE70() },
-			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_UE70 },
+			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE40() },
+			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_3A },
 			preset: true,
 			speedPset: true,
 			timePset: false,
@@ -319,10 +326,10 @@ export const SERIES_SPECS = [
 			OAF: true,
 			OTAF: true,
 			iris: true,
-			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_UE150() },
+			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_UE150 },
 			shut: false, // TODO: Add it's own shutter "OSJ:06:"
-			ped: false, // TODO: Add it's own Pedestal "OSJ:0F:"
-			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_UE150() },
+			ped: { cmd: 'OSJ:0F:', dropdown: c.CHOICES_PEDESTAL_UE150() },
+			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_3 },
 			preset: true,
 			speedPset: true,
 			timePset: true,
@@ -338,8 +345,68 @@ export const SERIES_SPECS = [
 	},
 
 	{
+		// Specific for the UE150 Series
+		id: 'UE160',
+		variables: {
+			version: true,
+			error: true,
+			ins: true,
+			power: true,
+			colorbar: true,
+			tally: true,
+			tally2: true,
+			tally3: true,
+			OAF: true,
+			iris: true,
+			gainValue: true,
+			preset: true,
+			whiteBalance: true,
+			colorTemperature: false,
+		},
+		feedbacks: {
+			powerState: true,
+			colorbarState: true,
+			tallyState: true,
+			tally2State: true,
+			tally3State: true,
+			insState: true,
+			autoFocus: true,
+			autoIris: true,
+			whiteBalance: true,
+			preset: true,
+		},
+		actions: {
+			panTilt: true,
+			ptSpeed: true,
+			zoom: true,
+			zSpeed: true,
+			focus: true,
+			fSpeed: true,
+			OAF: true,
+			OTAF: true,
+			iris: true,
+			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_UE160 },
+			shut: false, // TODO: Add it's own shutter "OSJ:06:"
+			ped: false, // TODO: Add it's own Pedestal "OSJ:0F:"
+			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_3 },
+			preset: true,
+			speedPset: true,
+			timePset: true,
+			power: true,
+			ins: true,
+			colorbar: true,
+			tally: true,
+			tally2: true,
+			tally3: true,
+			sdCard: false,
+			whiteBalance: true,
+			colorTemperature: false,
+		},
+	},
+
+	{
 		// Specific for the AW-HE2 Camera
-		id: 'AW-HE2', // A lot has been turned off since it will need custop setups for this camera, can be added if requested
+		id: 'HE2', // A lot has been turned off since it will need custop setups for this camera, can be added if requested
 		variables: {
 			version: true,
 			error: true,
@@ -393,7 +460,7 @@ export const SERIES_SPECS = [
 
 	{
 		// Specific for the AW-HE50 Camera
-		id: 'AW-HE50',
+		id: 'HE50',
 		variables: {
 			version: true,
 			error: true,
@@ -430,7 +497,7 @@ export const SERIES_SPECS = [
 			iris: true,
 			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HE50 },
 			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_HE50 },
-			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE50() },
+			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE40() },
 			filter: false,
 			preset: true,
 			speedPset: true,
@@ -447,7 +514,7 @@ export const SERIES_SPECS = [
 
 	{
 		// Specific for the AW-HE60 Camera
-		id: 'AW-HE60',
+		id: 'HE60',
 		variables: {
 			version: true,
 			error: true,
@@ -482,9 +549,9 @@ export const SERIES_SPECS = [
 			OAF: true,
 			OTAF: true,
 			iris: true,
-			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HE60() },
+			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HE50 },
 			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_HE60() },
-			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE60() },
+			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE40() },
 			filter: false,
 			preset: true,
 			speedPset: true,
@@ -501,7 +568,7 @@ export const SERIES_SPECS = [
 
 	{
 		// Specific for the AW-HE120 Camera
-		id: 'AW-HE120',
+		id: 'HE120',
 		variables: {
 			version: true,
 			error: true,
@@ -539,7 +606,7 @@ export const SERIES_SPECS = [
 			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HE120 },
 			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_HE120 },
 			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE120() },
-			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_HE120 },
+			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_3 },
 			preset: true,
 			speedPset: true,
 			timePset: false,
@@ -555,7 +622,7 @@ export const SERIES_SPECS = [
 
 	{
 		// Specific for the AW-HE130 Camera
-		id: 'AW-HE130',
+		id: 'HE130',
 		variables: {
 			version: true,
 			error: true,
@@ -592,8 +659,8 @@ export const SERIES_SPECS = [
 			iris: true,
 			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HE130 },
 			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_HE130 },
-			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE130() },
-			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_HE130 },
+			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE120() },
+			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_2 },
 			preset: true,
 			speedPset: true,
 			timePset: false,
@@ -609,7 +676,7 @@ export const SERIES_SPECS = [
 
 	{
 		// Specific for the AW-HR140 Camera
-		id: 'AW-HR140',
+		id: 'HR140',
 		variables: {
 			version: true,
 			error: true,
@@ -646,62 +713,8 @@ export const SERIES_SPECS = [
 			iris: true,
 			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HR140 },
 			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_HR140() },
-			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HR140() },
-			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_HR140() },
-			preset: true,
-			speedPset: true,
-			timePset: false,
-			power: true,
-			colorbar: true,
-			tally: true,
-			ins: true,
-			sdCard: false,
-			whiteBalance: true,
-			colorTemperature: false,
-		},
-	},
-
-	{
-		// Specific for the AW-HEF5 Camera
-		id: 'AW-HEF5',
-		variables: {
-			version: true,
-			error: true,
-			ins: true,
-			power: true,
-			colorbar: true,
-			tally: true,
-			OAF: true,
-			iris: true,
-			gainValue: true,
-			preset: true,
-			whiteBalance: true,
-			colorTemperature: false,
-		},
-		feedbacks: {
-			powerState: true,
-			colorbarState: true,
-			tallyState: true,
-			insState: true,
-			autoFocus: true,
-			autoIris: true,
-			whiteBalance: true,
-			preset: true,
-		},
-		actions: {
-			panTilt: true,
-			ptSpeed: true,
-			zoom: true,
-			zSpeed: true,
-			focus: true,
-			fSpeed: true,
-			OAF: true,
-			OTAF: true,
-			iris: true,
-			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_HE50 },
-			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_HE50 },
-			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE50() },
-			filter: true,
+			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_HE120() },
+			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_2 },
 			preset: true,
 			speedPset: true,
 			timePset: false,
@@ -717,7 +730,7 @@ export const SERIES_SPECS = [
 
 	{
 		// Specific for the AW-UE4 Camera
-		id: 'AW-UE4', // A lot has been turned off since it will need custop setups for this camera, can be added if requested
+		id: 'UE4', // A lot has been turned off since it will need custop setups for this camera, can be added if requested
 		variables: {
 			version: false,
 			error: true,
@@ -770,62 +783,8 @@ export const SERIES_SPECS = [
 	},
 
 	{
-		// Specific for the AW-SHU01 Camera
-		id: 'AW-SHU01',
-		variables: {
-			version: true,
-			error: true,
-			ins: true,
-			power: true,
-			colorbar: true,
-			tally: true,
-			OAF: true,
-			iris: true,
-			gainValue: true,
-			preset: true,
-			whiteBalance: true,
-			colorTemperature: false,
-		},
-		feedbacks: {
-			powerState: true,
-			colorbarState: true,
-			tallyState: true,
-			insState: true,
-			autoFocus: true,
-			autoIris: true,
-			whiteBalance: true,
-			preset: true,
-		},
-		actions: {
-			panTilt: true,
-			ptSpeed: true,
-			zoom: true,
-			zSpeed: true,
-			focus: true,
-			fSpeed: true,
-			OAF: true,
-			OTAF: true,
-			iris: true,
-			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_UE70() },
-			shut: { cmd: 'OSH:', dropdown: c.CHOICES_SHUTTER_UE70() },
-			ped: { cmd: 'OTP:', dropdown: c.CHOICES_PEDESTAL_UE70() },
-			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_UE70 },
-			preset: true,
-			speedPset: true,
-			timePset: false,
-			power: true,
-			colorbar: true,
-			tally: true,
-			ins: true,
-			sdCard: false,
-			whiteBalance: true,
-			colorTemperature: false,
-		},
-	},
-
-	{
 		// Specific for the AK-UB300 Camera
-		id: 'AK-UB300',
+		id: 'UB300',
 		variables: {
 			version: false,
 			error: true,
@@ -846,7 +805,7 @@ export const SERIES_SPECS = [
 			insState: false,
 			autoFocus: false,
 			autoIris: false,
-			whiteBalance: true,
+			whiteBalance: false,
 			preset: false,
 		},
 		actions: {
@@ -861,8 +820,8 @@ export const SERIES_SPECS = [
 			iris: false,
 			gain: { cmd: 'OGS:', dropdown: c.CHOICES_GAIN_UB300 },
 			shut: { cmd: 'OSG:5D:', dropdown: c.CHOICES_SHUTTER_UB300 },
-			ped: false, // TODO: Add UB300 Pedestal "OSG:4A:"
-			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_UB300 },
+			ped: { cmd: 'OSG:4A:', dropdown: c.CHOICES_PEDESTAL_UB300() },
+			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_3 },
 			preset: false,
 			speedPset: false,
 			timePset: false,
@@ -920,8 +879,8 @@ export const SERIES_SPECS = [
 			iris: true,
 			gain: { cmd: 'OGU:', dropdown: c.CHOICES_GAIN_CX350 },
 			shut: false,
-			ped: { cmd: 'OSJ:0F:', dropdown: c.CHOICES_PEDESTAL_CX350() },
-			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_CX350() },
+			ped: { cmd: 'OSJ:0F:', dropdown: c.CHOICES_PEDESTAL_UE150() },
+			filter: { cmd: 'OFT:', dropdown: c.CHOICES_FILTER_3 },
 			preset: false,
 			speedPset: false,
 			timePset: false,
